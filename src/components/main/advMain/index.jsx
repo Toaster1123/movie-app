f7441cbfbbaf9325c1ef9b5e0231cab8cb1a33d2
Toaster <prokhorov.rostislav@gmail.com>
2024-10-17 00:01:13 +0300

    edit main scroll

--- src/components/main/advMain/index.jsx
+++ src/components/main/advMain/index.jsx
@@ -3,11 +3,11 @@ import style from './adv.module.scss';
 export const AdvMain = () => {
   return (
     <div className={style.Main}>
-      <img src="" />
+      {/* <img src="" /> */}
       <div className={style.TextBlock}>
         <div className={style.subscrube}>
           <div className={style.subText}>Подписка</div>
-          <div className={style.subimg}>Прайм</div>
+          <img src="/img/prime.png" alt="" />
         </div>
         <div className={style.advBigText}>Смотрите мультики, спорт и кино</div>
         <div className={style.advYellowPrice}>Месяц за 1 ₽, затем месяц за 99 ₽</div>
